d6bb3b6712958f1529e800838d7075714b6d28e0
Alicia Weenum <weenum.alicia@gmail.com>
2020-10-22 12:53:04 -0400

    commit tree 5b28b288ea6a0e8c6e3ee80f5915534d9b631963

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+    counter = 0
+    new_array = [] #give value before while loop bc putting inside the while loop and everytime you loop, the variable will be reset
+    while counter < array.length
+        new_value = yield(array[counter])
+        new_array.push(new_value)
+        counter += 1
+    end
+    new_array
+end
 
